ff0fc5d9aaf60f6634afc11a9082356420c1a747
stephenn33 <40546167+stephenn33@users.noreply.github.com>
2024-09-19 01:34:58 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'foliant>=1.0.5',
         'jinja2',
-        'pydbml>=1.0.6',
+        'pydbml<6.0.0',
         'foliantcontrib.utils>=1.0.2',
     ],
     classifiers=[
